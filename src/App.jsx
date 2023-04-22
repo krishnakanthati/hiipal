697d22c382564855af67836dbc0fbe036a88e28b
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-04-23 00:32:51 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <h1>Hii pal!</h1>
