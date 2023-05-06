61e9a932c85224acbfaa864b8932c79dad380b3e
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-05-07 04:23:16 +0530

    navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 
 function Home() {
-  return (
-    <div>
-      <h1>Hii Pal!</h1>
-      <Link to="/page">
-        <button>page</button>
-      </Link>
-      <Link to={"/page/1"}>
-        <button>page 1</button>
-      </Link>
-      <Link to={"/page/2"}>
-        <button>page 2</button>
-      </Link>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default Home;
