d48ad8602fbef1da1a927677b9e205898d3e0142
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-05-20 23:14:20 +0530

    login

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,17 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return (
-    <div className="footer">
-      <p
-        style={{
-          color: "red",
-        }}
-      >
-        <span style={{ color: "black" }}>support: </span>hiipal@hotmail.com
-      </p>
-    </div>
-  );
+  return <div className="footer">hiipal@hotmail.com</div>;
 }
 
 export default Footer;
