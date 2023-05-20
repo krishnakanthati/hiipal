58642efba5aa327670d9888d3462efe298c30635
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-05-21 00:57:33 +0530

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,7 +4,145 @@ import "./Login.css";
 function Login() {
   return (
     <>
-      <div className="login-form"></div>
+      <div className="login-form-div">
+        <form action="" className="form-container">
+          <div className="form-group">
+            <label htmlFor="email" className="form-label">
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 0 }}
+              >
+                p
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 1 }}
+              >
+                a
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 2 }}
+              >
+                l
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 3 }}
+              >
+                &nbsp;
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 4 }}
+              >
+                I
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 5 }}
+              >
+                D
+              </span>
+              <span className="sr-only">pal ID</span>
+            </label>
+            <div className="form-group__input">
+              <input
+                required
+                type="email"
+                id="email"
+                className="form-input"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$"
+                title="Enter valid email address"
+                placeholder="Enter valid email address"
+              />
+              <div className="form-group__error">Enter a valid Pal ID</div>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="form-label">
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 0 }}
+              >
+                p
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 1 }}
+              >
+                a
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 2 }}
+              >
+                s
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 3 }}
+              >
+                s
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 4 }}
+              >
+                w
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 5 }}
+              >
+                o
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 5 }}
+              >
+                r
+              </span>
+              <span
+                aria-hidden="true"
+                className="label__letter"
+                style={{ "--index": 5 }}
+              >
+                d
+              </span>
+              <span className="sr-only">password</span>
+            </label>
+            <div className="form-group__input">
+              <input
+                required
+                type="password"
+                id="password"
+                className="form-input"
+                title="Enter correct password"
+                placeholder="Enter correct password"
+              />
+              <div className="form-group__error">Enter correct password</div>
+            </div>
+          </div>
+          <div className="form-group"></div>
+          <button type="submit" className="form-submit-button">
+            Welcome
+          </button>
+        </form>
+      </div>
     </>
   );
 }
