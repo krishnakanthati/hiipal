53befdc298ccab71b83751b417fcf468217b0001
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-06-24 22:24:15 +0530

    styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer">hiipal@hotmail.com</div>;
+  return <div className="footer">hiipal@hotmail.com&nbsp;</div>;
 }
 
 export default Footer;
