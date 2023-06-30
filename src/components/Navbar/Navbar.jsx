df3d892b0e91fbbaf905a205612bef51ad8e59f9
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-07-01 02:16:26 +0530

    styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar() {
           <ul className="header-ul">
             <li className="header-li">
               <Link className="nav-link" to="/" style={{ color: "black" }}>
-                Hii pal
+                Hii pal 👻
                 <span style={{ color: "#CCCCFF" }}>!</span>
               </Link>
             </li>
