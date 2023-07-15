9bf9bcae1881533e87b6a39987773ca1959bc732
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-07-16 00:01:24 +0530

    styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
   return (
     <div className="footer">
       <div className="pal-count">
-        pal: {palRegisteredCount} (<span style={{ color: "#00c50b" }}>+0</span>)
+        {palRegisteredCount} (<span style={{ color: "#00c50b" }}>+0</span>)
       </div>
       <div className="email">hiipal@hotmail.com</div>
     </div>
