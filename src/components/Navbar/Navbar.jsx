c6ce46b4b68890d2860360ac0aca7c4142885d44
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-07-29 00:23:43 +0530

    styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,9 +8,8 @@ import * as HiIcons from "react-icons/hi";
 import * as BiIcons from "react-icons/bi";
 import * as IoIcons2 from "react-icons/io5";
 import * as TbIcons from "react-icons/tb";
-import * as MdIcons from "react-icons/md";
 import * as RxIcons from "react-icons/rx";
-import * as RiIcons from "react-icons/ri";
+import * as AiIcons from "react-icons/ai";
 
 function Navbar({ isLoading }) {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -58,7 +57,7 @@ function Navbar({ isLoading }) {
             </li>
             <li className="navbar-li">
               <Link className="nav-link" to="/notifications">
-                <MdIcons.MdOutlineNotifications
+                <AiIcons.AiOutlineFire
                   className="nav-icon"
                   title="Notifications"
                 />
