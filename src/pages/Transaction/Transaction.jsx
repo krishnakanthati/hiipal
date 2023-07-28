525451b2b101b8519353fa555289efa4bccaef9c
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-07-29 02:36:53 +0530

    styling

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Transaction.css";
 import ExampleComponent from "../../components/ExampleComponent/ExampleComponent";
-import Footer from "../../components/Footer/Footer";
 
 function Transaction() {
   return (
     <>
       <Navbar />
       <div className="main-container">{/* <ExampleComponent /> */}</div>
-      <Footer />
     </>
   );
 }
