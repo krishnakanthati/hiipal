018a2188a8204bbfd53ca161394557131346a99c
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-07-30 21:17:34 +0530

    styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./Footer.css";
 
+import * as BiIcons from "react-icons/bi";
+import * as IoIcons from "react-icons/io5";
+
 function Footer() {
   const [palRegisteredCount, setPalRegisteredCount] = useState(null);
 
@@ -19,7 +22,18 @@ function Footer() {
 
   return (
     <div className="footer">
-      <div className="email">dev@hiipal.com</div>
+      <div className="footer-icon">
+        <div>
+          <BiIcons.BiSolidLike />
+        </div>
+        <div>
+          <BiIcons.BiShare />
+        </div>
+        <div>
+          <IoIcons.IoShareSocialSharp />
+        </div>
+      </div>
+      <div className="email">hiipal.com &copy; 2023</div>
     </div>
   );
 }
