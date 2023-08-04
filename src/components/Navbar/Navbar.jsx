67b1cd56345a3c15a7ee62df320d84cd04108e1b
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-08-05 00:25:21 +0530

    styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import diamond from "../../assets/diamond.mp3";
 import * as HiIcons from "react-icons/hi";
 import * as BiIcons from "react-icons/bi";
 import * as IoIcons2 from "react-icons/io5";
-import * as TbIcons from "react-icons/tb";
+import * as PiIcons from "react-icons/pi";
 import * as GiIcons from "react-icons/gi";
 import * as RxIcons from "react-icons/rx";
 import * as AiIcons from "react-icons/ai";
@@ -83,7 +83,10 @@ function Navbar({ isLoading }) {
             </li>
             <li className="navbar-li">
               <Link className="nav-link" to="/message">
-                <TbIcons.TbMessage title="Message" className="nav-icon" />
+                <IoIcons2.IoChatboxOutline
+                  title="Message"
+                  className="nav-icon"
+                />
               </Link>
             </li>
           </ul>
