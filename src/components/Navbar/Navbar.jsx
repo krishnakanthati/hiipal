30070d7c8ca1c4c7ac28e0157c8d1a76f880d9d9
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-08-05 01:10:38 +0530

    styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import * as HiIcons2 from "react-icons/hi2";
 import * as HiIcons from "react-icons/hi";
 import * as BiIcons from "react-icons/bi";
 import * as IoIcons2 from "react-icons/io5";
-import * as PiIcons from "react-icons/pi";
+import * as TiIcons from "react-icons/ti";
 import * as GiIcons from "react-icons/gi";
 import * as RxIcons from "react-icons/rx";
 import * as AiIcons from "react-icons/ai";
@@ -84,7 +84,7 @@ function Navbar({ isLoading }) {
             </li>
             <li className="navbar-li">
               <Link className="nav-link" to="/message">
-                <BiIcons.BiChat className="nav-icon" title="Message" />
+                <BiIcons.BiMessage className="nav-icon" title="Message" />
               </Link>
             </li>
           </ul>
