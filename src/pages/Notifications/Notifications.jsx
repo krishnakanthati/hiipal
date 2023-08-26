be797f3eff5703f39bd2928bffcbcf21934b1a04
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-08-26 11:15:34 +0530

    styling

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -2,40 +2,17 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 
+import "./Notifications.css";
+
 function Notifications() {
-  const test = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await fetch("http://localhost:8888/post/test", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({ name: "joe" }),
-      });
-
-      const data = await response.json();
-      if (data.status === "green") {
-        // Handle successful registration here
-        console.log("Test successful!");
-        console.log(data.token);
-      } else {
-        // Handle registration failure here
-        console.error("Registration failed:", data.error);
-      }
-    } catch (error) {
-      // Handle fetch error or other exceptions
-      console.error("Error:", error);
-    }
-  };
-
   return (
     <>
       <Navbar />
       <div className="main-container">
-        <button onClick={test}>Post Test</button>
-        <Link to="http://localhost:8888/get/profile">
-          <button>Get Test</button>
-        </Link>
+        <div className="choice">
+          <div className="div-a">A</div>
+          <div className="div-b">B</div>
+        </div>
       </div>
     </>
   );
