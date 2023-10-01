2f48ce98e901046b1720e485f9d99176705eb4db
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-10-01 22:28:41 +0530

    authentication

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,12 +7,21 @@ function SearchBar({ onVariableChange }) {
   const { isAuthenticated } = useContext(AuthContext);
   const [query, setQuery] = useState("");
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchbar");
+
+    searchInput.addEventListener("focus", function () {
+      window.location.href = "#/search";
+    });
+  });
+
   return (
     <div className="searchbar-div">
       <input
         type="search"
         value={query}
         className="searchbar"
+        id="searchbar"
         onChange={(e) => {
           setQuery(e.target.value);
           onVariableChange(e.target.value);
