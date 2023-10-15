3f640e1c627a16f843e29dcb97e61690591b235d
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-10-15 20:16:27 +0530

    authentication

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Message.css";
 
@@ -7,6 +7,19 @@ import * as BsIcons from "react-icons/bs";
 import * as FaIcons from "react-icons/fa6";
 
 function Message() {
+  const inputRef = useRef();
+
+  const handleInputChange = () => {
+    const inputValue = inputRef.current.value;
+    const messageSend = document.querySelector(".message-send");
+
+    if (inputValue.trim() !== "") {
+      messageSend.style.color = "#00c50b";
+    } else {
+      messageSend.style.color = "black";
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -16,7 +29,12 @@ function Message() {
             <BsIcons.BsEmojiLaughing />
           </div>
           <div className="message-div">
-            <input className="message-input" placeholder="Message" />
+            <input
+              className="message-input"
+              placeholder="Message"
+              ref={inputRef}
+              onInput={handleInputChange}
+            />
             <span className="message-send">
               <FaIcons.FaArrowUp />
             </span>
