ef7bb8d2edd35ac046916a69f1d58ebd1b06f4f0
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-10-15 20:03:45 +0530

    authentication

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -4,7 +4,7 @@ import "./Message.css";
 
 import * as HiIcons from "react-icons/hi2";
 import * as BsIcons from "react-icons/bs";
-import * as IoIcons from "react-icons/io5";
+import * as FaIcons from "react-icons/fa6";
 
 function Message() {
   return (
@@ -15,10 +15,12 @@ function Message() {
           <div className="message-emoji">
             <BsIcons.BsEmojiLaughing />
           </div>
-          <input className="message-input" placeholder="Message" />
-          <span className="message-send">
-            <IoIcons.IoSend />
-          </span>
+          <div className="message-div">
+            <input className="message-input" placeholder="Message" />
+            <span className="message-send">
+              <FaIcons.FaArrowUp />
+            </span>
+          </div>
           <div className="message-mic">
             <HiIcons.HiMiniMicrophone />
           </div>
